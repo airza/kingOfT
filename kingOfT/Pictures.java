858bb5a368cbe99869deea2d2bcca75bb730168d
airza <argentage@gmail.com>
2012-10-10 15:04:02 -0500

    merge pictures into main branch

---
+++ kingOfT/Pictures.java
@@ -0,0 +1,18 @@
+package kingOfT;
+
+import java.awt.Image;
+import java.io.InputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class Pictures {
+	public void dicePictures(){
+	InputStream dice01 = getClass().getResourceAsStream("dice01.jpg");
+	}
+}
+
+
+//test
