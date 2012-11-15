acfcc8ba19cc600f664719c6bbe16e55e5d68a0a
airza <argentage@gmail.com>
2012-11-15 13:13:48 -0600

    Update kingOfT/MainLoop.java
    Add comments

--- kingOfT/MainLoop.java
+++ kingOfT/MainLoop.java
@@ -6,7 +6,7 @@ public class MainLoop {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		//there should be some code here to handle beginning a new game, but there isn't
 		final int PLAYER_NUM = 6;
 		final String[] NAMES = {"A","B","C","D","E","F"};
 		Window win  = new Window();
