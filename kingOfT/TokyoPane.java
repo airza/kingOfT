e9279708813f7521476094a16b1b0a10ef66748d
airza <argentage@gmail.com>
2012-11-28 19:54:29 -0600

    add terrible art assets

---
+++ kingOfT/TokyoPane.java
@@ -0,0 +1,15 @@
+package kingOfT;
+
+import javax.swing.JPanel;
+
+public class TokyoPane {
+	private TokyoArea tokyoArea;
+	private JPanel tokyoAreaHolder;
+	private JPanel notTokyoAreaHolder;
+	ImageIcon icon = createImageIcon("images/M1.png");
+	public TokyoPane(TokyoArea tokyoArea) {
+		this.tokyoArea = tokyoArea;
+	}
+	
+	
+}
